8a890aec15b2954cb950af8abe83aae81dfcf449
radiantdelta <agentello007@gmail.com>
2024-05-14 02:11:27 +0300

    Beginning

---
+++ src/main/java/com/radiantdelta/sbertask/SberTaskApplication.java
@@ -0,0 +1,13 @@
+package com.radiantdelta.sbertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radiantdelta/sbertask/SberTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radiantdelta.sbertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
